a13c6d04d8e0942d7cfc7a652373562b08b8a832
Akash-Rana-25 <akashrana.work.gmail.com>
2023-03-22 13:40:55 +0530

    adedd

---
+++ Practical-3/Practical-3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Practical_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team T=new Team();
+            T.printinfo();
+        }
+    }
+    class Sponsor
+    {
+        protected string owner = "Rakuteen";
+    }
+    class Team : Sponsor
+    {
+        private string teamName="hi";
+
+        /// <summary>
+        /// This method concate  two Strings and display Full string
+        /// </summary>
+        public void printinfo()
+        {
+            string fullString;
+            fullString = owner+ " " + teamName;
+            Console.WriteLine(fullString);
+        }
+    }
+}
